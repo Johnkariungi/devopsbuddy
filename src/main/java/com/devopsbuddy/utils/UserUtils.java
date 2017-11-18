c70bf13d264e572239363b3178c2799961c04d2a
jonzie <jonzie@John-Kariungis-MacBook-Pro.local>
2017-11-18 13:18:29 -0800

    Enabled Spring security for the website

---
+++ src/main/java/com/devopsbuddy/utils/UserUtils.java
@@ -0,0 +1,17 @@
+package com.devopsbuddy.utils;
+
+public class UserUtils {
+	 /**
+     * Non instantiable.
+     */
+    private UserUtils() {
+        throw new AssertionError("Non instantiable");
+    }
+
+    /**
+     * Creates a user with basic attributes set.
+     * @param username The username.
+     * @param email The email.
+     * @return A User entity
+     */
+}
--- src/main/resources/static/js/devopsbuddy.js
+++ src/main/resources/static/js/devopsbuddy.js
@@ -1,12 +1,13 @@
 $( document ).ready( main );
 
 /* display image text about on click details */
-function main () {
-	
-	$('btn-collapse').click(function (e) {
-		e.preventDefault();
-		var $this = $(this);
-		var $collapse = $this.closest('.collapse-group').find('.collapse');
-		$collapse.collapse('toggle');
-	});
+
+function main() {
+
+    $('.btn-collapse').click(function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        var $collapse = $this.closest('.collapse-group').find('.collapse');
+        $collapse.collapse('toggle');
+    });
 }
