c5b59e372dc4278a5e0989d65fefdae571c4d265
jonzie <jonzie@John-Kariungis-MacBook-Pro.local>
2017-11-18 13:27:14 -0800

    fix merge chanagesfor reset head

--- src/main/java/com/devopsbuddy/config/SecurityConfig.java
+++ src/main/java/com/devopsbuddy/config/SecurityConfig.java
@@ -1,19 +1,13 @@
 package com.devopsbuddy.config;
 
 import java.security.SecureRandom;
-<<<<<<< HEAD
 import java.util.Arrays;
 import java.util.List;
-=======
->>>>>>> security
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-<<<<<<< HEAD
 import org.springframework.core.env.Environment;
-=======
->>>>>>> security
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,20 +18,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-<<<<<<< HEAD
-	@Autowired
-    private Environment env;
-	
-	/** The encryption SALT. */
-   private static final String SALT = "fdalkjalk;3jlwf00sfaof";
-    
-	@Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(12, new SecureRandom(SALT.getBytes()));
-    }
-	
 	/** Public URLs. */
-=======
+
 	/*@Autowired
     private UserSecurityService userSecurityService;
 
@@ -53,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     /** Public URLs. */
->>>>>>> security
+
     private static final String[] PUBLIC_MATCHERS = {
             "/webjars/**",
             "/css/**",
@@ -62,45 +44,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/",
             "/about/**",
             "/contact/**",
-<<<<<<< HEAD
             "/error/**/*",
-            "/console/**"
-=======
             "/error/**/*"
->>>>>>> security
+
     };
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-<<<<<<< HEAD
-    
-    		List<String> activeProfiles = Arrays.asList(env.getActiveProfiles()); /*get the profiles*/
-        if (activeProfiles.contains("dev")) {/* disable if inMemory DB for dev*/
-            http.csrf().disable();
-            http.headers().frameOptions().disable();
-        }
-        
-	    	http
-	        .authorizeRequests()
-	        .antMatchers(PUBLIC_MATCHERS).permitAll()
-	        .anyRequest().authenticated()
-	        .and()
-	        .formLogin().loginPage("/login").defaultSuccessUrl("/payload")
-	        .failureUrl("/login?error").permitAll()
-	        .and()
-	        .logout().permitAll();
-	}
-	
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-		.inMemoryAuthentication()
-		.withUser("user").password("password")
-		.roles("USER");
-	        //.userDetailsService(userSecurityService)
-	       // .passwordEncoder(passwordEncoder());
-	}
-=======
 
        /*List<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
         if (activeProfiles.contains("dev")) {
@@ -128,6 +78,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 /*.userDetailsService(userSecurityService)
                 .passwordEncoder(passwordEncoder());*/
     }
-            
->>>>>>> security
 }
