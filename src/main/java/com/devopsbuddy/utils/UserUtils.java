bb29d52dbc8bea56975771a98e21fd4f307d2c97
jonzie <jonzie@John-Kariungis-MacBook-Pro.local>
2017-11-29 21:12:50 -0800

    Created the frontend domain model and translation layer for the basic account

--- src/main/java/com/devopsbuddy/utils/UserUtils.java
+++ src/main/java/com/devopsbuddy/utils/UserUtils.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import com.devopsbuddy.backend.persistence.domain.backend.User;
 import com.devopsbuddy.web.controllers.ForgotMyPasswordController;
+import com.devopsbuddy.web.domain.fontend.BasicAccountPayload;
 
 public class UserUtils {
     /**
@@ -57,5 +58,21 @@ public class UserUtils {
 				"&token=" +
 				token;
 		return passwordResetUrl;
+	}
+
+	public static <T extends BasicAccountPayload> User fromWebUserToDomainUser(T frontendPayload) {
+		
+		User user = new User();
+		user.setUsername(frontendPayload.getUsername());
+		user.setPassword(frontendPayload.getPassword());
+		user.setFirstName(frontendPayload.getFirstName());
+		user.setLastName(frontendPayload.getLastName());
+		user.setEmail(frontendPayload.getEmail());
+		user.setPhoneNumber(frontendPayload.getPhoneNumber());
+		user.setCountry(frontendPayload.getCountry());
+		user.setEnabled(true);
+		user.setDescription(frontendPayload.getDescription());
+		
+		return user;
 	}
 }
--- src/test/java/com/devopsbuddy/test/Unit/UserUtilsUnitTest.java
+++ src/test/java/com/devopsbuddy/test/Unit/UserUtilsUnitTest.java
@@ -6,19 +6,26 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 
+import com.devopsbuddy.backend.persistence.domain.backend.User;
 import com.devopsbuddy.utils.UserUtils;
 import com.devopsbuddy.web.controllers.ForgotMyPasswordController;
+import com.devopsbuddy.web.domain.fontend.BasicAccountPayload;
 
 import junit.framework.Assert;
+import uk.co.jemos.podam.api.*;/*PodamFactory*/
 
 public class UserUtilsUnitTest {
 	
 	private MockHttpServletRequest mockHttpServletRequest;
+	
+	private PodamFactory podamFactory;
 
 	@Before
 	public void init() { 
 		mockHttpServletRequest = new MockHttpServletRequest(); 
+		podamFactory = new PodamFactoryImpl();
 	}
+	
 	
 	@SuppressWarnings("deprecation")
 	@Test
@@ -33,6 +40,25 @@ public class UserUtilsUnitTest {
 		String actualUrl = UserUtils.createPasswordResetUrl(mockHttpServletRequest, userId, token);
 		
 		Assert.assertEquals(expectedUrl, actualUrl);
+		
+	}
+	@SuppressWarnings("deprecation")
+	@Test
+	public void mapWebUserToDomainUser() {
+		BasicAccountPayload webUser = podamFactory.manufacturePojoWithFullData(BasicAccountPayload.class);
+		webUser.setEmail("me@example.com");
+		
+		User user = UserUtils.fromWebUserToDomainUser(webUser);
+		Assert.assertNotNull(user);
+		
+		Assert.assertEquals(webUser.getUsername(), user.getUsername());
+		Assert.assertEquals(webUser.getPassword(), user.getPassword());
+		Assert.assertEquals(webUser.getFirstName(), user.getFirstName());
+		Assert.assertEquals(webUser.getLastName(), user.getLastName());
+		Assert.assertEquals(webUser.getEmail(), user.getEmail());
+		Assert.assertEquals(webUser.getPhoneNumber(), user.getPhoneNumber());
+		Assert.assertEquals(webUser.getCountry(), user.getCountry());
+		Assert.assertEquals(webUser.getDescription(), user.getDescription());
 		
 	}
 }
